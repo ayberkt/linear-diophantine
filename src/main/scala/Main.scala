548478c618b11d8e8fe15b9910b33f7a71be1f1d
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-19 11:01:52 -0500

    Implement `prod`

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,4 +8,8 @@ object Main {
     (n : Int) =>
       List.fill(n)(n) map ((i : Int) => vector(i)(List.fill(i)(0)))
 
+  def prod : Vector[Int] => Vector[Int] => Int =
+    v1 => v2 => v1.indices.map((i : Int) => v1(i) * v2(i)).sum
+
+
 }

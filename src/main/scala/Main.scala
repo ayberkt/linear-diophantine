66e8ea7c79d652fc00d86a35644ef08447146737
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-19 16:39:33 -0500

    Make `basis` return a Set

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,9 +3,8 @@ object Main {
 
   def vector = (x : Int) => (elems : List[Int]) => (elems take x).toVector
 
-  def basis =
-    (n : Int) =>
-      List.fill(n)(n) map ((i : Int) => vector(i)(List.fill(i)(0)))
+  def basis : Int => Set[Vector[Int]] =
+    n => (List.fill(n)(n) map ((i : Int) => vector(i)(List.fill(i)(0)))).toSet
 
   def prod : Vector[Int] => Vector[Int] => Int =
     v1 => v2 => v1.indices.map((i : Int) => v1(i) * v2(i)).sum

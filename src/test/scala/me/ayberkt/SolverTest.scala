8f2675c741bfe430cc92d01e388c599ea92f7a6f
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-07-21 19:33:15 -0500

    Modify tests so that they take much shorter time

--- src/test/scala/me/ayberkt/SolverTest.scala
+++ src/test/scala/me/ayberkt/SolverTest.scala
@@ -4,14 +4,25 @@ import org.scalatest._
 class solveLinDiaphSpec extends FreeSpec {
   "x + 2y = 500" in {
     val cfs = List(1, 2)
-    val rhs = 2000
-    assert(solveLinDiaph(List(1, 2), 2000)
-      .forall(solveLinDiaph.check(cfs)(rhs)))
+    val rhs = 200
+    assert(solveLinDiaph(cfs, rhs).forall(solveLinDiaph.check(cfs)(rhs)))
   }
+
   "10x + 30y = 780" in {
     val cfs = List(10, 30)
     val rhs = 780
-    assert(solveLinDiaph(List(10, 30), 780)
-      .forall(solveLinDiaph.check(cfs)(rhs)))
+    assert(solveLinDiaph(cfs, rhs).forall(solveLinDiaph.check(cfs)(rhs)))
+  }
+
+  "3x + 5y + 12z = 780" in {
+    val cfs = List(3, 5, 12)
+    val rhs = 65
+    assert(solveLinDiaph(cfs, rhs).forall(solveLinDiaph.check(cfs)(rhs)))
+  }
+
+  "20a + 12b + 26c + 2d = 50" in {
+    val cfs = List(20, 12, 26, 2)
+    val rhs = 150
+    assert(solveLinDiaph(cfs, rhs).forall(solveLinDiaph.check(cfs)(rhs)))
   }
 }
